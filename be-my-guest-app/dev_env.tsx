c22eb0a62432d9e8bbb9282d6c2796516ca01f58
LAPTOP-SORM8G9A\Giulia <giuliaanno95@hotmail.it>
2023-04-27 15:46:15 +0200

    updated endpoints

--- be-my-guest-app/dev_env.tsx
+++ be-my-guest-app/dev_env.tsx
@@ -1,5 +1,6 @@
 class EnvVars {
-  serverUrl: string = "https://26cp3qcyh5.execute-api.eu-west-3.amazonaws.com/calculator/add/5/2";
+  //serverUrlCalculator: string = "https://26cp3qcyh5.execute-api.eu-west-3.amazonaws.com/calculator/add/5/2";
+  serverUrl: string = "https://be-my-guest.jordangottardo.com/api/v1";
 }
 
 export default new EnvVars();
